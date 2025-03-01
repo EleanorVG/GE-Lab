c006824f130c6491da2b4bb0bbd83f8edf0f0cc3
EleanorVG <134004397+EleanorVG@users.noreply.github.com>
2025-03-01 15:19:48 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -8,9 +8,55 @@ const navToggle = document.getElementById('nav-toggle')
 navToggle.addEventListener('click', ()=> {
     navMenu.classList.toggle('show-menu');
     navToggle.classList.toggle('animate-toggle')
+})
+
+/*  Remove Menu Mobile  */
+
+const navLink = document.querySelectorAll('.nav-link');
+
+const linkAction = () => {
+  const navMenu = document.getElementById('nav-menu');
+
+  navToggle.classList.remove('animate-toggle');
+  navMenu.classList.remove('show-menu')
+};
+
+navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+/* Change background header */
+
+const scrollHeader = () => {
+  const header = document.getElementById('header')
+
+  this.scrollY >= 20 
+  ? header.classList.add('bg-header') 
+  : header.classList.remove('bg-header');
 }
 
-)
+window.addEventListener('scroll', scrollHeader)
+
+/*  Scroll Sections Active Link  */
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+    sectionTop = current.offsetTop - 58,
+    sectionId = current.getAttribute('id'),
+    sectionsClass = document.querySelector('.nav-menu a[href*=' + sectionId + ']')
+  
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link');
+    } else {
+      sectionsClass.classList.remove('active-link');
+    }
+  });
+};
+
+window.addEventListener('scroll', scrollActive);
+
 
 /* MIXITUP Filter Publications */
 var mixer = mixitup('.publication-container', {
@@ -52,11 +98,11 @@ var researchSwiper = new Swiper(".research-swiper", {
       },
 
       breakpoints: {
-        /*768*/250: {
+        768: {
           slidesPerView: 2,
           },
-        /*1024: {
+        1023: {
           slidesPerView: 3,          
-        },*/
+        },
       },
 });
